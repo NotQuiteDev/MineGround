120cea0cad50ee2485066ee21b8b58ef8cffffd6
sfour <dlcksgur15@naver.com>
2024-08-15 11:01:06 +0900

    0.11.10 불꽃놀이 개선

--- src/main/java/org/battle/mineground/WorldBorderController.java
+++ src/main/java/org/battle/mineground/WorldBorderController.java
@@ -396,6 +396,7 @@ public class WorldBorderController implements Listener{
         }
     }
 
+
     private void celebrateWinner(Player winner) {
         // 모든 플레이어에게 우승자 메시지 타이틀로 표시
         for (Player player : Bukkit.getOnlinePlayers()) {
@@ -404,44 +405,65 @@ public class WorldBorderController implements Listener{
                     10, 70, 20); // 타이틀 표시 (10틱 딜레이, 70틱 표시, 20틱 페이드 아웃)
         }
 
-        // 폭죽 발사
+        // 불꽃 발사
         fireworkTask = new BukkitRunnable() {
             @Override
             public void run() {
+                // 게임이 종료된 상태라도 불꽃 발사를 계속
+                for (int i = 0; i < random.nextInt(4) + 1; i++) { // 한 번에 1~4개의 불꽃 발사
+                    Location location = winner.getLocation();
+                    Firework firework = location.getWorld().spawn(location, Firework.class);
+                    FireworkMeta meta = firework.getFireworkMeta();
+                    meta.addEffect(randomFireworkEffect()); // 랜덤 불꽃 효과 추가
+                    meta.setPower(random.nextInt(3) + 1); // 1~3까지의 랜덤 파워
+                    firework.setFireworkMeta(meta);
 
-                // 우승자의 위치에서 폭죽 생성
-                Location location = winner.getLocation();
-                Firework firework = winner.getWorld().spawn(location, Firework.class);
-                FireworkMeta meta = firework.getFireworkMeta();
-                meta.addEffect(FireworkEffect.builder()
-                        .withColor(Color.RED)
-                        .withColor(Color.YELLOW)
-                        .with(FireworkEffect.Type.BALL_LARGE)
-                        .withFlicker()
-                        .withTrail()
-                        .build());
-                meta.setPower(2);
-                firework.setFireworkMeta(meta);
+
+                }
             }
         };
 
-        // 폭죽을 30초 동안 발사
-        fireworkTask.runTaskTimer(plugin, 0L, 40L); // 40틱(2초) 간격으로 실행
-        Bukkit.getScheduler().runTaskLater(plugin, fireworkTask::cancel, 600L); // 30초 후 폭죽 중단
+        // 불꽃을 랜덤하게 발사 (랜덤 시간 간격: 10틱 ~ 40틱)
+        fireworkTask.runTaskTimer(plugin, 0L, random.nextInt(31) + 10); // 10~40틱 간격으로 실행
+        Bukkit.getScheduler().runTaskLater(plugin, fireworkTask::cancel, 600L); // 30초 후 불꽃 중단
 
         // 파티클 효과 추가
         particleTask = new BukkitRunnable() {
             @Override
             public void run() {
-
+                // 게임이 종료된 상태라도 30초 동안 파티클 생성
                 Location location = winner.getLocation();
-                winner.getWorld().spawnParticle(Particle.TOTEM, location, 200, 1, 1, 1, 0.1);
+                location.getWorld().spawnParticle(Particle.TOTEM, location, 200, 1, 1, 1, 0.1);
             }
         };
 
         // 파티클을 30초 동안 생성
         particleTask.runTaskTimer(plugin, 0L, 20L); // 20틱(1초) 간격으로 실행
         Bukkit.getScheduler().runTaskLater(plugin, particleTask::cancel, 600L); // 30초 후 파티클 중단
+    }
+
+    private FireworkEffect randomFireworkEffect() {
+        FireworkEffect.Builder builder = FireworkEffect.builder();
+
+        // 랜덤 색상 선택 (최소 2개의 색상)
+        int colorCount = random.nextInt(3) + 2; // 2~4개의 색상
+        for (int i = 0; i < colorCount; i++) {
+            builder.withColor(Color.fromRGB(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+        }
+
+        // 랜덤 불꽃 타입 선택
+        FireworkEffect.Type[] types = FireworkEffect.Type.values();
+        builder.with(types[random.nextInt(types.length)]);
+
+        // 랜덤으로 깜빡임과 꼬리 효과 추가
+        if (random.nextBoolean()) {
+            builder.withFlicker();
+        }
+        if (random.nextBoolean()) {
+            builder.withTrail();
+        }
+
+        return builder.build();
     }
 
     private void stopCelebration() {
